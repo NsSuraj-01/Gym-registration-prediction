b2487de9df6aa98e6a46030de638429171db8e40
NsSuraj-01 <saisuraj1510@gmail.com>
2023-11-24 00:05:52 +0530

    edited medical form

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/stats.html");
@@ -32,6 +34,6 @@ app.post("/", (req, res) => {
   res.send("Your BMI: " + index + "<br>Status: " + result(index));
 })
 
-app.listen(1000, () => {
+app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 })
