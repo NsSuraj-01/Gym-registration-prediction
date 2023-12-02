c13a4f7f2e3a7fe598535c35df96db7af98f9e19
NsSuraj-01 <saisuraj1510@gmail.com>
2023-12-03 00:07:07 +0530

    added medical form to take input data

--- app.js
+++ app.js
@@ -1,87 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const path = require("path");
-// const mongoose = require("mongoose");
-// const ejs = require("ejs");
-// const _ = require("lodash");
-
-
-// const app = express();
-// app.set('view engine', 'ejs');
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }))
-// app.use(express.static(('public')));
-
-
-// // ----- Database ----- 
-// const dbName = "Students"
-// const dbURI = "mongodb://127.0.0.1:27017/" +dbName;
-
-// mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then( () => console.log("connected to " + dbName + " database"))
-// .catch( (err) => console.log(err))
-
-// // schema
-// const resultSchema = new mongoose.Schema(
-//   {
-//     "StudentID": Number,
-//     "FirstName": String,
-//     "LastName" : String,
-//     "Age": Number,
-//     "Gender": String,
-//     "Height": Number,
-//     "Weight": Number,
-//     "BloodPressure": String,
-//     "Cholesterol": Number,
-//     "HeartRate": Number,
-//     "TestDate": String,
-//     "TestResult": String
-//   }
-// )
-
-// // model
-// const Result = mongoose.model("Result", resultSchema);
-
-// // count number of documents
-// async function countDocs() {
-//   const num = await Result.countDocuments();
-//   console.log(num);
-// }
-
-// const stInfo = [];
-// // find query
-// async function find(key, value) {
-//   const results = await Result.find({[key] : value})
-//   results.forEach( (item) => {
-//     stInfo.push(item);
-//   });
-//   console.log(stInfo);
-// }
-
-
-
-// // server
-
-// app.get("/", (req, res) => {
-//   res.render("getData");
-// })
-
-// app.post("/getData", (req, res) => {
-//   const sid = req.body.id;
-//   find("StudentID" ,sid);
-//   res.render("result");
-// })
-
-// app.get("/result", (req, res) => {
-//   res.render("result", {
-//     results : stInfo
-//   })
-// })
-
-// app.listen(3000, () => {
-//   console.log("Server is running on Port 3000");
-// })
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -131,6 +47,23 @@ async function find(key, value) {
   return stInfo;
 }
 
+async function insert(key, value, doc) {
+  await Result.insert({[key] : value}, doc);
+}
+
+async function findAndReplace(key, value, doc) {
+  const res = await Result.findOneAndReplace({[key] : value}, doc, {upsert: true})
+  if(res === null) {
+    console.log("Inserted")
+  } else {
+    console.log("Updated")
+  }
+}
+
+async function getCount() {
+  
+}
+
 // Server
 app.get("/", (req, res) => {
   res.render("getData");
@@ -140,11 +73,30 @@ app.get("/form", (req, res) => {
   res.render("form");
 })
 
+app.post("/form", (req, res) => {
+  const data = req.body;
+  const sid = req.body.StudentID;
+  // console.log(data, sid);
+  findAndReplace("StudentID", sid, data);
+  res.render("getData")
+})
+
+app.get("/getData", (req, res) => {
+  res.render("getData")
+})
+
 app.post("/getData", async (req, res) => {
   const sid = req.body.id;
   const stInfo = await find("StudentID", sid);
   res.render("result", { results: stInfo });
 });
+
+app.get("/gym-data", (req, res) => {
+  const total_count = getCount();
+  res.render("gym-data", {
+    count : total_count
+  })
+})
 
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
